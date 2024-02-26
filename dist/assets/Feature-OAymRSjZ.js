934cf41891a8a1e1ab95874787c91083c29a8a32
Patrick Sinco <psinco@gmail.com>
2024-02-26 10:51:58 -0600

    Build app

---
+++ dist/assets/Feature-OAymRSjZ.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,_ as r}from"./index-3KiBpRvC.js";function o(n,c,s,p,u,f){return t(),e("h1",null,"hiyafromfeature")}const a={setup(){return{}}},l=r(a,[["render",o]]);export{l as default};
