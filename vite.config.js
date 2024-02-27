9036633d2ab4209a7935463560b6228e217e5a5a
Patrick Sinco <psinco@gmail.com>
2024-02-27 10:10:28 -0600

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/roundupper-vue3/',
+  base: '/roundupper/',
   plugins: [
     vue(),
   ],
