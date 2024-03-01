18732a33d349316fb410acd30aa341cb3b9ffae3
Patrick Sinco <psinco@gmail.com>
2024-03-01 09:15:32 -0600

    Make a build

---
+++ dist/assets/TopStory-bHgItkjN.js
@@ -0,0 +1,1 @@
+import{c as o,o as t,_ as e}from"./index-bHBSUCkW.js";function r(s,c,a,p,u,_){return t(),o("h1",null,"hiyafromtopstory")}const n={setup(){return{}}},l=e(n,[["render",r]]);export{l as default};
