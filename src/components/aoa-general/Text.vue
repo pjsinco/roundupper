1a91929f5eee1916fc2dcc363082d8078c9c6dfd
Patrick Sinco <psinco@gmail.com>
2024-03-08 11:54:44 -0500

    Get make-link button working

--- src/components/aoa-general/Text.vue
+++ src/components/aoa-general/Text.vue
@@ -1,9 +1,10 @@
 <script>
 import { ref, computed, onMounted } from 'vue';
+
 import Workspace from '@/components/Workspace.vue';
 import { copyHtml, copyText } from '@/composables/useButtonFunctions';
 import { useRendererForAoaGeneral } from '@/composables/renderer-aoa-general';
-import { editorFromTextArea } from '@/composables/useEditorFromTextArea';
+import { useEditorFromTextArea } from '@/composables/useEditorFromTextArea';
 import { marked } from 'marked';
 
 export default {
@@ -17,6 +18,7 @@ export default {
     const defaultInput = `# Lorem ipsum dolor sit amet\nConsectetur adipisicing **elit**, sed do *eiusmod* [tempor incididunt](https://osteopathic.org) ut labore et dolore magna aliqua.\n`;
     const input = ref(defaultInput);
     const centerText = ref(false);
+    let editor = null;
 
     const { renderer } = useRendererForAoaGeneral();
 
@@ -44,7 +46,7 @@ export default {
 
     function initEditor() {
       const el = document.getElementById('input');
-      const editor = editorFromTextArea(input, el, 'calc(100vh - 425px)');
+      editor = useEditorFromTextArea(input, el, 'calc(100vh - 425px)');
     }
 
     function copy() {
@@ -59,6 +61,39 @@ export default {
       input.value = defaultInput;
     }
 
+    function makeLink(evt) {
+      const selection = editor.getSelection();
+      const { line, ch } = editor.getCursor('anchor');
+
+      // Make sure there are no new-lines in selection
+      if (selection.indexOf('\n') > 0) {
+        return;
+      }
+
+      // Check immediately-surrounding chars to see if
+      // they're [ && ], which would mean there's already
+      // a link there.
+      const linkCheckRange = editor.getRange(
+        { line, ch: ch - 1 },
+        { line, ch: ch + selection.length + 1 }
+      );
+      const firstChar = linkCheckRange.charAt(0);
+      const lastChar = linkCheckRange.charAt(linkCheckRange.length - 1);
+      if (firstChar == '[' && lastChar == ']') {
+        return;
+      }
+
+      const startOfStrToHighlight = ch + selection.length + ']('.length + 1;
+      const endOfStrToHighlight = startOfStrToHighlight + 'url'.length;
+
+      editor.replaceSelection('[' + selection + '](url)');
+      editor.setSelection(
+        { line, ch: startOfStrToHighlight },
+        { line, ch: endOfStrToHighlight }
+      );
+      editor.focus();
+    }
+
     onMounted(initEditor);
 
     return {
@@ -71,6 +106,7 @@ export default {
       reset,
       copy,
       copyTextVersion,
+      makeLink,
     };
   },
 };
@@ -82,4 +118,4 @@ export default {
     include ../../views/aoa-general/renders/text
 </template>
 
-<style scoped></style>
+<style></style>
